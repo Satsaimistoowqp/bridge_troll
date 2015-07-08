Satsaimistoowqp/bridge_troll

commit ae68c64a70ed503e57bbf9c2310c4a5c6e1d127c

Reorder fields on event page to reduce jumpiness when toggling radio button
Add checkbox-submit-disabling logic to crazy JS toggler code

Move crazy js toggler code out of events.js and into its own file

app/assets/javascripts/events.js.coffee
@@ -25,29 +25,6 @@ setUpExclusiveCheckboxes = ($el) ->
         if (checkbox.id != e.target.id)
           $(checkbox).prop('checked', false)
 
-setUpToggles = ->
-  getSelectedValue = ($input) ->
-    if $input.attr('type') == 'radio'
-      $("input[type=radio][name='#{$input.attr('name')}']:checked").val()
-
-  inputChanged = ->
-    $input = $(this)
-    visibleValue = $input.data('toggle-show-when').toString()
-    selector = $input.data('toggle-target')
-    checked = getSelectedValue($input) == visibleValue
-
-    $togglables = $(".#{selector}")
-    if (checked)
-      $togglables.toggle(true)
-      $togglables.find('input, select').prop('disabled', false)
-    else
-      $togglables.toggle(false)
-      $togglables.find('input, select').prop('disabled', true)
-
-  $('[data-toggle-target]').on('change', inputChanged)
-  $('[data-toggle-target]').each (ix, el) ->
-    inputChanged.call(el)
-
 setupRemoveSessions = ->
   if $('.remove-session').length
     $(document).on 'click', '.remove-session > a', (e)->
@@ -65,22 +42,12 @@ jQuery ->
   $('.select2-dropdown').select2(width: 'element')
 
   setUpDatePicker($('.datepicker'))
-  setUpExclusiveCheckboxes($('body'))
-  setUpToggles()
 
   $(document).on 'nested:fieldAdded', (event) ->
     $field = event.field
     $dateField = $field.find('.datepicker')
     setUpDatePicker($dateField)
     setUpExclusiveCheckboxes($field)
-
-  cocChanged = ->
-    $el = $('#coc')
-    if ($el.length > 0)
-      $('.btn-submit').prop('disabled', !$el[0].checked)
-
-  $('#coc').on('change', cocChanged)
-  cocChanged()
 
   $('.chapter-select').on 'change', (event) ->
     chapterId = $(this).val()

Gemfile
@@ -35,7 +35,7 @@ end
 gem 'handlebars_assets'
 gem 'jquery-datatables-rails'
 gem 'sass-rails'
-gem 'compass-rails', github: 'danielpclark/compass-rails' # https://github.com/Compass/compass-rails/issues/195
+gem 'compass-rails', github: 'compass/compass-rails' # awaiting compass-rails 2.0.2
 gem 'coffee-rails'
 gem 'uglifier'
 gem 'bootstrap-sass'

app/assets/javascripts/toggling.js.coffee
@@ -1,0 +1,26 @@
+setUpToggles = ->
+  getSelectedValue = ($input) ->
+    if $input.attr('type') == 'radio'
+      $("input[type=radio][name='#{$input.attr('name')}']:checked").val()
+
+  inputChanged = ->
+    $input = $(this)
+    selector = $input.data('toggle-target')
+    $togglables = $(".#{selector}")
+
+    if $input.data('toggle-show-when')
+      visibleValue = $input.data('toggle-show-when').toString()
+      checked = getSelectedValue($input) == visibleValue
+
+      $togglables.toggle(checked)
+      $togglables.find('input, select').prop('disabled', !checked)
+
+    if $input.data('toggle-enable-when-checked')
+      $togglables.prop('disabled', !$input.prop('checked'))
+
+  $('[data-toggle-target]').on('change', inputChanged)
+  $('[data-toggle-target]').each (ix, el) ->
+    inputChanged.call(el)
+
+jQuery ->
+  setUpToggles()

app/assets/stylesheets/_base.scss
@@ -229,7 +229,6 @@ ul.bulleted {
   }
 
   .add_nested_fields {
-    margin-bottom: 30px;
     outline: none;
     text-decoration: none;
   }

app/views/events/_form.html.erb
@@ -3,6 +3,21 @@
 
   <h2>Basic Event Information</h2>
 
+  <h3>Event Type</h3>
+  <div class="field non-bolded">
+    <div>
+      <%= label_tag do %>
+        <%= f.radio_button :allow_student_rsvp, true, data: {'toggle-target' => 'workshop-only', 'toggle-show-when' => true} %> Students and Volunteers (normal workshop)
+      <% end %>
+    </div>
+    <div>
+      <%= label_tag do %>
+        <%= f.radio_button :allow_student_rsvp, false, data: {'toggle-target' => 'workshop-only', 'toggle-show-when' => true} %> Just Volunteers (work party, teacher
+        training)
+      <% end %>
+    </div>
+  </div>
+
   <div class="field">
     <%= f.input :title %>
   </div>
@@ -68,57 +83,32 @@
     <% end %>
   </div>
 
-  <h2 class="mb-10-only">Who can attend?</h2>
-
-  <h3>Event Type</h3>
-  <div class="field non-bolded">
-    <div>
-      <%= label_tag do %>
-        <%= f.radio_button :allow_student_rsvp, true, data: {'toggle-target' => 'workshop-only', 'toggle-show-when' => true} %> Students and Volunteers (normal workshop)
-    <% end %>
-    </div>
-    <div>
-      <%= label_tag do %>
-        <%= f.radio_button :allow_student_rsvp, false, data: {'toggle-target' => 'workshop-only', 'toggle-show-when' => true} %> Just Volunteers (work party, teacher
-        training)
-    <% end %>
-    </div>
-  </div>
-
-  <h3>Announcement Email</h3>
-
-  <% unless @event.published? %>
-    <div class="field non-bolded">
-      <%= f.label :email_on_approval do %>
-        <%= f.check_box :email_on_approval %> Send announcement email to chapter when the event is approved
-      <% end %>
-    </div>
-  <% end %>
-
-  <div class="field workshop-only">
-    <h3>Ask for plus-one host name?</h3>
-    <p>Historically, *Bridge workshops for women have allowed men to attend as the guest of a woman. If you want to do a similar +1 thing, you can ask attendees to list who they are the guest of.</p>
-      <p>The plus-one host form says this:</p>
-    <blockquote>
-      <p>All workshops on Bridge Troll focus on providing opportunities for under-represented populations in tech. If you are not a member of an underrepresented group, you can only attend as a student if you're the guest of someone who is a member of the workshop's target population. (For example, men can come as the guest of a woman, a straight person can attend as the guest of a gay person, etc.)
-      </p>
-      <p>If you are not a member of this workshop's target demographic, list the name of the person who is bringing you here: [text field]
-      </p>
-      </blockquote>
-    <p>If your workshop isn't open to +1s, you can choose not to show that field.</p>
-    <div>
-      <%= label_tag do %>
-        <%= f.radio_button :plus_one_host_toggle, true %> Show plus-one host name form
-      <% end %>
-    </div>
-    <div>
-      <%= label_tag do %>
-        <%= f.radio_button :plus_one_host_toggle, false %> Don't show plus-one host name form
-      <% end %>
-    </div>
-  </div>
-
   <div class='workshop-only'>
+    <h2>Student details</h2>
+
+    <div class="field">
+      <h3>Ask for plus-one host name?</h3>
+      <p>Historically, *Bridge workshops for women have allowed men to attend as the guest of a woman. If you want to do a similar +1 thing, you can ask attendees to list who they are the guest of.</p>
+        <p>The plus-one host form says this:</p>
+      <blockquote>
+        <p>All workshops on Bridge Troll focus on providing opportunities for under-represented populations in tech. If you are not a member of an underrepresented group, you can only attend as a student if you're the guest of someone who is a member of the workshop's target population. (For example, men can come as the guest of a woman, a straight person can attend as the guest of a gay person, etc.)
+        </p>
+        <p>If you are not a member of this workshop's target demographic, list the name of the person who is bringing you here: [text field]
+        </p>
+        </blockquote>
+      <p>If your workshop isn't open to +1s, you can choose not to show that field.</p>
+      <div>
+        <%= label_tag do %>
+          <%= f.radio_button :plus_one_host_toggle, true %> Show plus-one host name form
+        <% end %>
+      </div>
+      <div>
+        <%= label_tag do %>
+          <%= f.radio_button :plus_one_host_toggle, false %> Don't show plus-one host name form
+        <% end %>
+      </div>
+    </div>
+
     <h3>Capacity</h3>
 
     <p>
@@ -160,6 +150,16 @@
     <%= f.input :student_details, label: 'Student Details', input_html: {rows: 4} %>
   </div>
 
+  <h2>Announcement Email</h2>
+
+  <% unless @event.published? %>
+    <div class="field non-bolded">
+      <%= f.label :email_on_approval do %>
+        <%= f.check_box :email_on_approval %> Send announcement email to chapter when the event is approved
+      <% end %>
+    </div>
+  <% end %>
+
   <% if @event.new_record? %>
     <h2>Ready to create your Event? Hold on a second...</h2>
 
@@ -171,7 +171,10 @@
 
     <div class="field">
       <%= label_tag :coc, class: 'question' do %>
-        <strong><%= check_box_tag :coc, '1', params[:coc] %>
+        <strong><%= check_box_tag :coc, '1', params[:coc], data: {
+          'toggle-target' => 'coc-required',
+          'toggle-enable-when-checked' => true
+        } %>
           I accept the <a href="http://bridgefoundry.org/code-of-conduct/" target="_blank">Code of Conduct</a> and will communicate it at the beginning of the event.
         </strong>
       <% end %>
@@ -179,6 +182,6 @@
   <% end %>
 
   <div class="actions">
-    <%= f.submit class: 'btn btn-submit' %>
+    <%= f.submit class: 'btn btn-submit coc-required' %>
   </div>
 <% end %>

app/views/rsvps/_form.html.erb
@@ -136,7 +136,10 @@
    <% if @rsvp.new_record? %>
       <div class="checkbox">
         <%= label_tag :coc, class: 'question' do %>
-          <%= check_box_tag :coc, '1', params[:coc] %> I accept the <a href="http://bridgefoundry.org/code-of-conduct/" target="_blank">Code of Conduct</a>
+          <%= check_box_tag :coc, '1', params[:coc], data: {
+            'toggle-target' => 'coc-required',
+            'toggle-enable-when-checked' => true
+          } %> I accept the <a href="http://bridgefoundry.org/code-of-conduct/" target="_blank">Code of Conduct</a>
         <% end %>
       </div>
     <%end%>
@@ -145,7 +148,7 @@
       <%= f.hidden_field :event_id %>
       <%= f.hidden_field :role_id %>
       <% unless @rsvp_preview_mode %>
-        <%= f.submit "Submit", class: 'btn btn-submit' %>
+        <%= f.submit "Submit", class: 'btn btn-submit coc-required' %>
       <% end %>
     </div>
   <% end %>

commit 01db9df2a7d4a8f4d05aca563d1eceb56997e38e

Fix regression causing exclusive-checkbox behavior to not work for initial event session
This meant that "Required for Students" and "Volunteers Only!" could be
checked simultaneously without toggling the other one off.

It still would cause a validation error, so nbd.

Regressed since ae68c64a7

app/assets/javascripts/events.js.coffee
@@ -53,6 +53,7 @@ jQuery ->
   $('.select2-dropdown').select2(width: 'element')
 
   setUpDatePicker($('.datepicker'))
+  setUpExclusiveCheckboxes($('body'))
 
   $(document).on 'nested:fieldAdded', (event) ->
     $field = event.field